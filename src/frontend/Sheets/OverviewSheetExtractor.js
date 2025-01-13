1671e8268bf3edd91a6268e03e8146c70a73e86e
Hamish Arnold <info@assessmentbot.com>
2025-01-13 15:46:31 +0000

    Added attempt at collecting data from all sheets. Will test later.

--- src/frontend/Sheets/OverviewSheetExtractor.js
+++ src/frontend/Sheets/OverviewSheetExtractor.js
@@ -11,6 +11,7 @@ class OverviewSheetExtractor extends BaseSheetManager {
     constructor() {
         super();
         this.sheetsData = [];
+        this.overviewData = {};
     }
 
     /**
@@ -35,6 +36,59 @@ class OverviewSheetExtractor extends BaseSheetManager {
         });
 
         return this.sheetsData;
+    }
+
+    /**
+     * Extracts data from a single Overview sheet
+     * @param {string} fileId - The spreadsheet file ID
+     * @returns {Array<Array>} Sheet values excluding last two rows
+     */
+    _extractSingleOverviewSheet(fileId) {
+        const spreadsheet = SpreadsheetApp.openById(fileId);
+        const overviewSheet = spreadsheet.getSheetByName('Overview');
+        
+        if (!overviewSheet) {
+            throw new Error(`Overview sheet not found in spreadsheet ${fileId}`);
+        }
+
+        const values = overviewSheet.getDataRange().getValues();
+        // Remove last two rows (blank row and class average)
+        return values.slice(0, -2);
+    }
+
+    /**
+     * Processes all sheets and combines data by year group and class
+     * @returns {Object} Combined overview data
+     */
+    processAllOverviewSheets() {
+        // Get details of all sheets first
+        this.getDetailsOfSheetsToExtractDataFrom();
+        
+        this.overviewData = this.sheetsData.reduce((accumulator, { fileId, name, yearGroup }) => {
+            try {
+                const sheetData = this._extractSingleOverviewSheet(fileId);
+                
+                if (!accumulator[yearGroup]) {
+                    accumulator[yearGroup] = {};
+                }
+                
+                accumulator[yearGroup][name] = sheetData;
+                
+            } catch (error) {
+                console.error(`Error processing sheet ${name}: ${error.message}`);
+            }
+            return accumulator;
+        }, {});
+
+        return this.overviewData;
+    }
+
+    /**
+     * Gets the combined overview data
+     * @returns {Object} The overview data organized by year group and class name
+     */
+    getOverviewData() {
+        return this.overviewData;
     }
 }
 
