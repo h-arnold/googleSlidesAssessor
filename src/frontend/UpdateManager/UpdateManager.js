c6ad16298ad98f4cc0b646c6a31fe1dbc7567766
Hamish Arnold <work@hamisharnold.co.uk>
2025-02-05 20:11:21 +0000

    Add global functions to launch and update Assessment Records Wizard in `z_main.js`

--- src/frontend/UpdateManager/UpdateManager.js
+++ src/frontend/UpdateManager/UpdateManager.js
@@ -454,22 +454,3 @@ class UpdateManager {
 
 // Code.gs
 
-/**
- * Global function to launch the Update Assessment Records Wizard.
- * Google Apps Script cannot call class methods directly so this function creates an instance
- * of UpdateManager and then calls the runAssessmentRecordUpdateWizard method.
- */
-function showUpdateAssessmentRecordWizard() {
-  const updateManager = new UpdateManager();
-  updateManager.runAssessmentRecordUpdateWizard();
-}
-
-/**
- * Global function called from the wizard when the user clicks "Finish".
- * This creates a new UpdateManager instance, loads the saved state, and then calls its updateAssessmentRecords method.
- */
-function updateAssessmentRecordsFromWizard() {
-  const updateManager = new UpdateManager();
-  updateManager.loadState();
-  updateManager.updateAssessmentRecords();
-}
